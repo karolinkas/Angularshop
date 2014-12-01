cf78b7f8a2af43b504132d08a0b5e997db85525f
Karolinka <li-nee@gmx.de>
2014-12-01 14:10:16 +0100

    being able to show comments

--- app.js
+++ app.js
@@ -3,7 +3,47 @@
 	var app = angular.module('store', [ ]);
 
 	app.controller("StoreController", function(){
-		this.products = gems;
+		this.products =[
+		{
+			name: "Tetraeder",
+			price: 2.95,
+			description: "This is the basic shape of a mineral.",
+			canPurchase: true,
+			soldOut: false,
+			reviews: [
+				{
+					stars:5,
+					body: "I love this product!", 
+					author: "joe@thomas.com"
+				},
+				{
+					stars:3,
+					body: "I like this product!", 
+					author: "me@steffi.com"
+				}
+			]
+		},
+		{
+			name: "Octaeder",
+			price: 5.10,
+			description: "This is the second basic shape of a mineral.",
+			canPurchase: true,
+			soldOut: false,
+			reviews: [
+				{
+					stars:5,
+					body: "I love this product!", 
+					author: "joe@thomas.com"
+				},
+				{
+					stars:3,
+					body: "I like this product!", 
+					author: "me@steffi.com"
+				}
+		]
+	}
+	]
+
 	});
 
 	app.controller("PanelController", function(){
@@ -19,21 +59,6 @@
 
 	});
 
-	var gems = [{
-		name: "Tetraeder",
-		price: 2.95,
-		description: "This is the basic shape of a mineral.",
-		canPurchase: true,
-		soldOut: false,
-	},
-	{
-		name: "Octaeder",
-		price: 5.10,
-		description: "This is the second basic shape of a mineral.",
-		canPurchase: true,
-		soldOut: false,
-	}
-	]
 
 
 
